8188ec7b8c41be7b238bcecb9a6a63fa7d8c0807
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2025-02-24 21:57:20 +0500

    commited

--- vantage/nuxt.config.ts
+++ vantage/nuxt.config.ts
@@ -1,8 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_KEY,
+    },
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["./assets/css/main.css"],
+  css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
   },
